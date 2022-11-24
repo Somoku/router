45fa86f7003206856c43edaa6205432eddbeb6af
Linsh <linsh0@protonmail.com>
2022-11-24 01:07:03 -0600

    [UPDATE] Basic framework built.

--- router.cpp
+++ router.cpp
@@ -1,13 +1,65 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include "router.h"
 
 RouterBase* create_router_object() {
     return new Router;
 }
 
+int Router::data_handler(int in_port, Header header, char* payload, char* packet){
+    return -1;
+}
+
+int Router::dv_handler(int in_port, Header header, char* payload, char* packet){
+    return -1;
+}
+
+int Router::control_handler(int in_port, Header header, char* payload, char* packet){
+    return -1;
+}
+
 void Router::router_init(int port_num, int external_port, char* external_addr, char* available_addr) {
+    this->port_num = port_num;
+    this->external_port = external_port;
+    if(external_port == 0){
+        this->external_addr = nullptr;
+        this->available_addr = nullptr;
+        this->available_mask = 0;
+        this->external_mask = 0;
+        return;
+    }
+    // Split CIDR external_addr into ip + mask
+    this->external_addr = strtok(external_addr, "/");
+    this->external_mask = atoi(strtok(NULL, "/"));
+
+    // Split CIDR available_addr into ip + mask
+    this->available_addr = strtok(available_addr, "/");
+    this->available_mask = atoi(strtok(NULL, "/"));
     return;
 }
 
 int Router::router(int in_port, char* packet) {
-    return 1;
+    // Analyze received packet.
+    Header header;
+    memcpy(&header, packet, HEADER_SIZE);
+    char* payload{new char[header.length]};
+    memcpy(payload, packet + HEADER_SIZE, header.length);
+
+    // Handle different types.
+    switch(header.type){
+        case TYPE_DV:
+            return dv_handler(in_port, header, payload, packet);
+        case TYPE_DATA:
+            return data_handler(in_port, header, payload, packet);
+        case TYPE_CONTROL:
+            return control_handler(in_port, header, payload, packet);
+        default:
+            fprintf(stderr, "Error: Invalid type.\n");
+    }
+    return -1;
 }
--- router.h
+++ router.h
@@ -1,7 +1,36 @@
 #include "router_prototype.h"
+#include <stdint.h>
+
+#define HEADER_SIZE 12
+#define MAX_PACKET 16384
+#define TYPE_DV 0x00
+#define TYPE_DATA 0x01
+#define TYPE_CONTROL 0x02
 
 class Router : public RouterBase {
+private:
+    int port_num; /* Port number of router.
+                   1 is preserved for controller */
+    int external_port; /* External network port number.
+                        0 = No connection to external network */
+    char* external_addr; /* Address range of external network.
+                              0 if external_port == 0 */
+    uint32_t external_mask; // Mask number of external_addr
+    char* available_addr; /* Available address range of public network.
+                           Null if external_port == 0 */
+    uint32_t available_mask; // Mask number of available_addr
+    
 public:
     void router_init(int port_num, int external_port, char* external_addr, char* available_addr);
     int router(int in_port, char* packet);
+    int data_handler(int in_port, Header header, char* payload, char* packet);
+    int dv_handler(int in_port, Header header, char* payload, char* packet);
+    int control_handler(int in_port, Header header, char* payload, char* packet);
+};
+
+struct Header {
+    uint32_t src;
+    uint32_t dst;
+    uint8_t type;
+    uint16_t length;
 };
